834691bf588037b527b84bd120a9d79387c0fbdd
ThomasTwomey <league@WTS15.attlocal.net>
2017-08-02 16:52:55 -0700

    Augusta 12

--- src/Houses.java
+++ src/Houses.java
@@ -1,27 +1,48 @@
+import java.awt.Color;
 import java.util.Random;
 
+import org.teachingextensions.logo.PenColors;
 import org.teachingextensions.logo.Tortoise;
 
 public class Houses {
 	public static void main(String[] args) {
+		
 		Random r = new Random();
-		int ran = r.nextInt(100) + 100;
+		int ran = r.nextInt(200) + 100;
+		
 		Tortoise.setX(10);
 		Tortoise.setY(400);
 		Tortoise.show();
+		Tortoise.setSpeed(10);
+		
+for(int i = 0; i<40;i++){
+			house();
+}
 	}
 
-	public void house() {
+	public static void house() {
+		Random rrr = new Random();
+		int rr = rrr.nextInt(2);
 		Random r = new Random();
 		int ran = r.nextInt(100) + 100;
+		Tortoise.setPenColor(PenColors.getRandomColor());
 		Tortoise.setAngle(0);
 		Tortoise.move(ran);
-		if (ran >= 150) {
-			Tortoise.setAngle(95);
+		if (rr >= 1) {
+			Tortoise.setAngle(45);
 			Tortoise.move(20);
-			Tortoise.setAngle(180);
-		} else if (ran < 150) {
-			Tortoise.setAngle(angle);
+			Tortoise.setAngle(140);
+			Tortoise.move(20);
+			
+		} else  {
+			Tortoise.setAngle(90);
+			Tortoise.move(20);
+			
 		}
+		Tortoise.setAngle(180);
+		Tortoise.move(ran);
+		Tortoise.setAngle(90);
+		Tortoise.setPenColor(Color.green);
+		Tortoise.move(20);
 	}
 }
--- src/NEtflixRunner.java
+++ src/NEtflixRunner.java
@@ -1,19 +1,42 @@
 
 public class NEtflixRunner {
-public static void main(String[] args) {
-	//1. Instantiate some Movie objects (at least 5).
-	Movie Austin = new Movie("Austin Powers in GoldMember", 4);	
-	Movie Jumpst = new Movie("21 Jump St", 5);
-	Movie Expelled = new Movie("Expelled", 4);
-	//2. Use the Movie class to get the ticket price of one of your movies.
-
-		//3. Instantiate a NetflixQueue.
-		//4. Add your movies to the Netflix queue.
-		//5. Print all the movies in your queue.
-		
-		//6. Use your NetflixQueue object to finish the sentence "the best movie is...."
-		//7. Use your NetflixQueue to finish the sentence "the second best movie is...." 
-
-
+	public static void main(String[] args) {
+		// 1. Instantiate some Movie objects (at least 5).
+		Movie Austin = new Movie("Austin Powers in GoldMember", 4);
+		Movie Jumpst = new Movie("ET", 5);
+		Movie Expelled = new Movie("Expelled", 4);
+		Movie BatnRob = new Movie("Batman and Robin", +1);
+		Movie Star = new Movie("Star Wars the Force Awakens", +2);
+		// 2. Use the Movie class to get the ticket price of one of your movies.
+		// Austin
+		System.out.println(Austin.getTitle() + " Price is " + Austin.getTicketPrice());
+		// Jumpst
+		System.out.println(Jumpst.getTitle() + " Price is " + Jumpst.getTicketPrice());
+		// Expelled
+		System.out.println(Expelled.getTitle() + " Price is " + Expelled.getTicketPrice());
+		// BatnRob
+		System.out.println(BatnRob.getTitle() + " Price is " + BatnRob.getTicketPrice());
+		// Star
+		System.out.println(Star.getTitle() + " Price is " + Star.getTicketPrice());
+		// 3. Instantiate a NetflixQueue.
+		NetflixQueue queue = new NetflixQueue();
+		// 4. Add your movies to the Netflix queue.
+		queue.addMovie(Star);
+		queue.addMovie(BatnRob);
+		queue.addMovie(Expelled);
+		queue.addMovie(Jumpst);
+		queue.addMovie(Austin);
+		// 5. Print all the movies in your queue.
+		queue.printMovies();
+		// 6. Use your NetflixQueue object to finish the sentence "the best
+		// movie is...."
+		System.out.println("The best movie is " + queue.getBestMovie() );
+		// 7. Use your NetflixQueue to finish the sentence "the second best
+		// movie is...."
+		queue.sortMoviesByRating();
+for(int i = 0; i<5;i++){
+		System.out.println(  queue.getMovie(i));
+	
+	}
 }
 }
