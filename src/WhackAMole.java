e7e131fab368455ab72dd07423e761337a413b48
FruitWave <league@WTS8.attlocal.net>
2017-10-25 16:57:03 -0700

    whackAmole,slot machine, picasso(ip)

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,8 +1,12 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,7 +14,7 @@ import javax.swing.JPanel;
 public class WhackAMole implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-
+	ImageIcon img;
 	public static void main(String[] args) {
 		new WhackAMole();
 	}
@@ -19,19 +23,31 @@ public class WhackAMole implements ActionListener {
 		
 		Random rann = new Random();
 		int ran = rann.nextInt(24);
-		
-		
+		try {
+			BufferedImage molePic = ImageIO.read(this.getClass().getResourceAsStream("screen.png"));
+		img= new ImageIcon(molePic);
+		}
+		catch(Exception e){
+			
+		}
+	
 		for (int i = 0; i < 24; i++) {
 			JButton button = new JButton();
-			button.setPreferredSize(new Dimension(100,100));
-			
+			button.setPreferredSize(new Dimension(100, 100));
+			panel.setBackground(Color.green);
+			button.setOpaque(true);
+			button.setBackground(Color.BLACK);
+
 			panel.add(button);
 			if (i == ran) {
-				button.setText("mole");
+				button.setIcon(img);
+				button.setText("a");
+				
 			}
 			button.addActionListener(this);
 
 		}
+		
 		frame.setSize(500, 700);
 		frame.add(panel);
 
@@ -41,10 +57,10 @@ public class WhackAMole implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-			JButton b = (JButton)e.getSource();
-			if(b.getText().equals("mole")){
-				frame.dispose();
-				new WhackAMole();
-			}
+		JButton b = (JButton) e.getSource();
+		if (b.getText().equals("a")) {
+			frame.dispose();
+			new WhackAMole();
+		}
 	}
 }
---
+++ src/colorPallete.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class colorPallete {
+private Color blue;
+private Color green;
+private Color red;
+	public colorPallete(Color c1, Color c2, Color c3) {
+		this.blue = c1;
+		this.green = c2;
+		this.red = c3;
+	}
+}
